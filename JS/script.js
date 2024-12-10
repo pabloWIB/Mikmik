f5ed10a426b071a50734cc6dbfc632e9311624ef
pabloWIB <pablonietoperez369@gmail.com>
2024-12-10 02:51:48 -0500

    Cambios

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Assign some jquery elements we'll need
+    var $swiper = $(".swiper-container");
+    var $bottomSlide = null;
+    var $bottomSlideContent = null;
+
+    var mySwiper = new Swiper(".swiper-container", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        centeredSlides: true,
+        roundLengths: true,
+        loop: true,
+        loopAdditionalSlides: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    });
+});
