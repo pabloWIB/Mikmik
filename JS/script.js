6de4457f2930b23332cbebb8f84e815e3c1dab7f
pabloWIB <pablonietoperez369@gmail.com>
2024-12-13 18:55:29 -0500

    Cambios

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     var mySwiper = new Swiper(".swiper-container", {
         spaceBetween: 10,
-        slidesPerView: 3,
+        slidesPerView: 4,
         centeredSlides: true,
         roundLengths: true,
         loop: true,
@@ -13,6 +13,32 @@ $(document).ready(function () {
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            // Para pantallas pequeñas (hasta 480px)
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 6,
+            },
+            // Para pantallas medianas (hasta 768px)
+            768: {
+                slidesPerView: 1.5,
+                spaceBetween: 7,
+            },
+            // Para pantallas grandes (mayores a 768px)
+            1024: {
+                slidesPerView: 2.5,
+                spaceBetween: 8,
+            },
+            // Para pantallas extra grandes (mayores a 1200px)
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+            },
+            1400: {
+                slidesPerView: 3.5,
+                spaceBetween: 12,
+            }
         }
     });
 });
